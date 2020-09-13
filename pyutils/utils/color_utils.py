c0893c7a4f36455a8a41f6f5be8e65670ea7b660
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-09-13 22:41:34 +0900

    Add Color Code Utils

--- pyutils/utils/color_utils.py
+++ pyutils/utils/color_utils.py
@@ -1,54 +1,122 @@
-class C3():
-    """ Color Code Converter """
-    def __init__(self, color, max_val=1, color_code=None):
-        self.color = color
-        self.max_val = max_val
-        self._init(color_code=color_code)
+#coding: utf-8
+from .generic_utils import handleTypeError
 
-    @property
-    def _supported_color_codes(self):
-        return ["rgba", "rgb", "hex"]
+SUPPORTED_COLOR_CODES = ["hex", "rgb", "rgba"]
 
-    def _init(self, color_code=None):
-        if (color_code is None) or (color_code not in self._supported_color_codes):
-            color_code = self.detect_color_code(color)
-        self.color_code = color_code
+def detect_color_code_type(color):
+    """Detect Color Code type
 
-    def detect_color_code(self, color):
-        if isinstance(color, str):
-            color_code = "hex"
-        elif isinstance(color, tuple) or isinstance(color, list):
-            color_code = {
-                3: "rgb",
-                4: "rgba",
-            }.get(len(color))
-        else:
-            raise TypeError(f"{toBLUE('color')} must be {toGREEN('str')} or {toGREEN('tuple')}, not {toRED(type())}")
-        return color_code
+    Args:
+        color (tuple / str): color code.
 
-    @staticmethod
-    def rgb2hex(rgb, max_val=1):
-        return "#"+"".join([format(int(255/max_val*e), '02x') for e in rgb]).upper()
+    Examples:
+        >>> detect_color_code_type("#FFFFFF")
+        'hex'
+        >>> detect_color_code_type((255,255,255))
+        'rgb'
+        >>> detect_color_code_type((0,0,0,1))
+        'rgba'
+    """
+    handleTypeError(types=[str, tuple, list], color=color)
+    if isinstance(color, str):
+        color_code = "hex"
+    elif isinstance(color, tuple) or isinstance(color, list):
+        color_code = {
+            3: "rgb",
+            4: "rgba",
+        }.get(len(color))
+    return color_code
 
-    @staticmethod
-    def rgba2rgb(rgba, max_val=1):
-        alpha = rgba[-1]
-        rgb = rgba[:-1]
-        type_ = int if max_val==255 else float
-        # compute the color as alpha against white
-        return tuple([type_(alpha*e+(1-alpha)*max_val) for e in rgb])
+def hex2rgb(hex, max_val=1):
+    """Convert color code from ``hex`` to ``rgb``"""
+    return tuple([int(hex[-6:][i*2:(i+1)*2], 16)/255*max_val for i in range(3)])
 
-    @staticmethod
-    def hex2rgb(hex, max_val=1):
-        tuple([int(hex[-6:][i*2:(i+1)*2], 16)/255*max_val for i in range(3)])
+def hex2rgba(hex, max_val=1):
+    """Convert color code from ``hex`` to ``rgba``"""
+    return rgb2rgba(rgb=hex2rgb(hex=hex[-6:], max_val=max_val), max_val=max_val)
 
-def choose_text_color(color, ctype="rgb", max_val=1):
-    color = {
-        "rgb"  : color,
-        "hex"  : 1,
-        "rgba" : rgba2rgb
-    }.get(ctype)
-    # Ref: WCAG (https://www.w3.org/TR/WCAG20/)
+def rgb2hex(rgb, max_val=1):
+    """Convert color code from ``rgb`` to ``hex``"""
+    return "#"+"".join([format(int(255/max_val*e), '02x') for e in rgb]).upper()
+
+def rgb2rgba(rgb, max_val=1):
+    """Convert color code from ``rgb`` to ``rgba``"""
+    return (*rgb, 1)
+
+def rgba2hex(rgba, max_val=1):
+    """Convert color code from ``rgba`` to ``hex``"""
+    return rgb2hex(rgb=rgba2rgb(rgba=rgba, max_val=max_val), max_val=max_val)
+
+def rgba2rgb(rgba, max_val=1):
+    """Convert color code from ``rgba`` to ``rgb``"""
+    alpha = rgba[-1]
+    rgb = rgba[:-1]
+    type_ = int if max_val==255 else float
+    # compute the color as alpha against white
+    return tuple([type_(alpha*e+(1-alpha)*max_val) for e in rgb])
+
+def _do_nothing(color, max_val=1):
+    return color
+
+def _toColorCode_create(to_color_code):
+    def toColorCode(color, max_val=1):
+        color_code = detect_color_code_type(color=color)
+        return {
+            color_code: globals().get(f"{color_code}2{to_color_code.lower()}", _do_nothing) 
+            for color_code in SUPPORTED_COLOR_CODES
+        }.get(color_code)(color, max_val=max_val)
+    
+    toColorCode.__doc__ = f"""Convert color code to {to_color_code.upper()}
+
+    Args:
+        color (tuple / str): color code.
+        
+    Examples:
+        >>> to{to_color_code.upper()}("#FFFFFF")
+        {toColorCode("#FFFFFF")}
+        >>> to{to_color_code.upper()}((255, 255, 255), max_val=255)
+        {toColorCode((255, 255, 255), max_val=255)}
+        >>> to{to_color_code.upper()}((1, 1, 1, 1), max_val=1)
+        {toColorCode((1, 1, 1, 1), max_val=1)}
+
+    """
+    return toColorCode
+
+toHEX  = _toColorCode_create("hex")
+toRGB  = _toColorCode_create("rgb")
+toRGBA = _toColorCode_create("rgba")
+
+def choose_text_color(color, max_val=1):
+    """Select an easy-to-read text color from the given color.
+
+    Args:
+        color (tuple / str): color code.
+
+    References: 
+        `WCAG <https://www.w3.org/TR/WCAG20/>`_
+
+    Examples:
+        >>> choose_text_color(color=(  0,   0,   0), max_val=255)
+        (255, 255, 255)
+        >>> choose_text_color(color=(  0,   0, 255), max_val=255)
+        (255, 255, 255)
+        >>> choose_text_color(color=(  0, 255,   0), max_val=255)
+        (0, 0, 0)
+        >>> choose_text_color(color=(  0, 255, 255), max_val=255)
+        (0, 0, 0)
+        >>> choose_text_color(color=(255,   0,   0), max_val=255)
+        (0, 0, 0)
+        >>> choose_text_color(color=(255,   0, 255), max_val=255)
+        (0, 0, 0)
+        >>> choose_text_color(color=(255, 255,   0), max_val=255)
+        (0, 0, 0)
+        >>> choose_text_color(color=(255, 255, 255), max_val=255)
+        (0, 0, 0)
+
+    """
+    color_code = detect_color_code_type(color=color)
+    rgb = toRGB(color=color)
+
     R,G,B = [e/max_val for e in rgb]
     # Relative Brightness BackGround.
     Lbg = 0.2126*R + 0.7152*G + 0.0722*B
@@ -58,4 +126,9 @@ def choose_text_color(color, ctype="rgb", max_val=1):
 
     Cw = (Lw + 0.05) / (Lbg + 0.05)
     Cb = (Lbg + 0.05) / (Lb + 0.05)
-    return (0,0,0) if Cb>Cw else (max_val,max_val,max_val)
+    text_rgb = (0,0,0) if Cb>Cw else (max_val,max_val,max_val)
+    return {
+        "rgb"  : _do_nothing,
+        "hex"  : rgb2hex,
+        "rgba" : _do_nothing,
+    }.get(color_code)(text_rgb, max_val=max_val)
