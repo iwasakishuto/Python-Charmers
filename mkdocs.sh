2f73c0ebc96faf13bad9fe99472b212774df2537
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-09-13 20:24:14 +0900

    Add logging.

--- mkdocs.sh
+++ mkdocs.sh
@@ -2,19 +2,41 @@
 # chmod +x mkdocs.sh
 # ./mkdocs.sh
 
+function logging() {
+  echo -e "\033[0;32m$1\033[0m"
+}
+
+function warnings() {
+  echo -e "\033[31m$1\033[0m"
+}
+
+function whereIam() {
+  echo -e "@ \033[07m`pwd`\033[0m"
+}
+
 SRC_DIRNAME="src"
 DOC_DIRNAME="docs"
+MODULE_DIRNAME="pyutils"
 CREATED_DIRNAME="_build/html"
+HERE=$(cd $(dirname $0);pwd)
 
-here=$(cd $(dirname $0);pwd) 
-cd $here
+logging "cd $HERE"
+cd $HERE
+whereIam
 
 if [ -d $DOC_DIRNAME ]; then
-  echo "Delete $DOC_DIRNAME directory."
+  warnings "Delete old $DOC_DIRNAME directory."
   rm -rf $DOC_DIRNAME
 fi
 
+logging "$ sphinx-apidoc -f -o $SRC_DIRNAME $MODULE_DIRNAME"
+sphinx-apidoc -f -o $SRC_DIRNAME $MODULE_DIRNAME
+
+logging "cd $SRC_DIRNAME"
 cd $SRC_DIRNAME
+whereIam
+
+logging "make html"
 make html
-mv $CREATED_DIRNAME $DOC_DIRNAME
-mv $DOC_DIRNAME ../$DOC_DIRNAME
+logging "mv $CREATED_DIRNAME ../$DOC_DIRNAME"
+mv $CREATED_DIRNAME ../$DOC_DIRNAME
