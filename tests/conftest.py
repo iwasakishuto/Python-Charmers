a3b5f0c334e66d05e905849ad5297c634e938896
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-09-15 19:34:05 +0900

    Add test codes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,20 +5,20 @@ import pytest
 from data import TestData
 import warnings
 try:
-    from gummy.utils._warnings import PythonUtilsImprementationWarning
+    from gummy.utils._warnings import PyUtilsImprementationWarning
 except ModuleNotFoundError:
     here     = os.path.abspath(os.path.dirname(__file__))
     REPO_DIR = os.path.dirname(here)
     sys.path.append(REPO_DIR)
-    print(f"You didn't install 'PythonUtils', so add {REPO_DIR} to search path for modules.")
-    from lib.utils._warnings import PythonUtilsImprementationWarning
+    print(f"You didn't install 'PyUtils', so add {REPO_DIR} to search path for modules.")
+    from lib.utils._warnings import PyUtilsImprementationWarning
 
 def pytest_addoption(parser):
     parser.addoption("--pyutils-warnings", choices=["error", "ignore", "always", "default", "module", "once"], default="ignore")
 
 def pytest_configure(config):
     action = config.getoption("pyutils_warnings")
-    warnings.simplefilter(action, category=PythonUtilsImprementationWarning)
+    warnings.simplefilter(action, category=PyUtilsImprementationWarning)
 
 @pytest.fixture
 def db():
