1445338235b12bb49704d3c4707c6d98ef4cbfd9
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-10-03 18:43:51 +0900

    Add render_template

--- pycharmers/cli/regexp_replacement.py
+++ pycharmers/cli/regexp_replacement.py
@@ -3,8 +3,8 @@ import os
 import re
 import sys
 import json
-import glob
 import argparse
+from pathlib import Path
 
 from ._path import PYCHARMERS_CLI_REGEXP_REPLACEMENT_DIR
 from ..utils._colorings import toBLUE, toGREEN
@@ -36,27 +36,39 @@ def regexp_replacement(argv=sys.argv[1:]):
 
     Examples:
 
-        .. code-block:: shell
+        - Replace Input file using regular expression.
+            .. code-block:: shell
 
-            # Replace Input file using regular expression.
-            $ regexp-replacement -I sample.md -jf sample.json
-                Json   file : /Users/iwasakishuto/.pycharmers/cli/regexp_replacement/labbooks.json
-                Input  file : sample.md
-                Output file : sample.md
-            # Show all json file in 'json-dir'
-            $ regexp-replacement --show-all
-            Json directory: /Users/iwasakishuto/.pycharmers/cli/regexp_replacement
-            * sample.json: Sample Json
-            # Show the content of the file at `json-path`
-            $ regexp-replacement -jf sample.json --show
-            Json:  /Users/iwasakishuto/.pycharmers/cli/regexp_replacement/sample.json
-            {
-                "description" : "Sample Json",
-                "patterns" : [
-                    ["`(.*?)`", "<span class=\"code\">\\1</span>"],
-                    ["+(.*?)+", "<span class=\"strong\">\\1</span>"]
-                ]
-            }
+                $ regexp-replacement -I sample.md -jf sample.json
+                    Json   file : /Users/iwasakishuto/.pycharmers/cli/regexp_replacement/labbooks.json
+                    Input  file : sample.md
+                    Output file : sample.md
+                $ regexp-replacement -I sample.md -jf sample.json -suf suffix
+                    Json: /Users/iwasakishuto/.pycharmers/cli/regexp_replacement/labbooks.json
+                    Input path : sample.md
+                    Output path: sample_suffix.md
+
+        - Show all json file in ``json-dir``
+
+            .. code-block:: shell
+
+                $ regexp-replacement --show-all
+                Json directory: /Users/iwasakishuto/.pycharmers/cli/regexp_replacement
+                * sample.json: Sample Json
+
+        - Show the content of the file at ``json-path``
+
+            .. code-block:: shell
+
+                $ regexp-replacement -jf sample.json --show
+                Json:  /Users/iwasakishuto/.pycharmers/cli/regexp_replacement/sample.json
+                {
+                    "description" : "Sample Json",
+                    "patterns" : [
+                        ["`(.*?)`", "<span class=\"code\">\\1</span>"],
+                        ["+(.*?)+", "<span class=\"strong\">\\1</span>"]
+                    ]
+                }
     """
     parser = argparse.ArgumentParser(prog="regexp-replace", add_help=True)
     parser.add_argument("-I",  "--input-path",  type=str, default=None, help="Path/to/input file.")
@@ -74,9 +86,10 @@ def regexp_replacement(argv=sys.argv[1:]):
     if args.show_all:
         json_dir = args.json_dir
         print(f"Json directory: {toGREEN(json_dir)}")
-        for fn in glob.glob(f"{json_dir}/*.json"):
-            with open(os.path.join(json_dir, fn)) as f:
-                print(f"* {toBLUE(os.path.basename(fn))}: {json.load(f).get('description')}")
+        p = Path(json_dir)
+        for fn in p.glob("**/*.json"):
+            with open(fn.absolute()) as f:
+                print(f"* {toBLUE(fn.name)}: {json.load(f).get('description')}")
         sys.exit(-1)
 
     # Show the content of the file at `json-path`
@@ -103,8 +116,8 @@ def regexp_replacement(argv=sys.argv[1:]):
             f_out.writelines([replace_str(line) for line in readlines])
 
     def add_suffix(path, suffix, sep="."):
-        *fp, ext = os.path.split(sep)
-        return sep.join(fp) + suffix + sep + ext
+        *fp, ext = path.split(sep)
+        return f"{sep.join(fp)}_{suffix}{sep}{ext}"
 
     input_path = args.input_path
     suffix = args.suffix
@@ -119,11 +132,14 @@ def regexp_replacement(argv=sys.argv[1:]):
         if output_dir is None:
             in2out = add_suffix
         else:
-            if output_dir.endswith("/"): output_dir = input_path[:-1]
+            if output_dir.endswith("/"): output_dir = output_dir[:-1]
             in2out = lambda path, suffix : path.replace(input_path, output_dir)
         
         print(f"Input dir : {toBLUE(input_path)}")
         print(f"Output dir: {toBLUE(output_path)}")
         ext = args.extension
-        for fn in glob.glob(f"{input_path}/**/*{ext}"):
-            replace_file(fn, in2out(fn))
+
+        p = Path(input_path)
+        for fp in p.glob(f"**/*{ext}"):
+            fp = str(fp)
+            replace_file(fp, in2out(fp, suffix))
