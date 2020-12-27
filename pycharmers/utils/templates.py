1475a000bf42dbc078997ab6096519fc9c1e494c
iwasakishuto <cabernet.rock@gmail.com>
2020-12-28 04:03:33 +0900

    Added a template for creating function with reStructuredText documents.

--- pycharmers/utils/templates.py
+++ pycharmers/utils/templates.py
@@ -2,15 +2,36 @@
 import os
 from ._path import TEMPLATES_DIR
 from ._colorings import toBLUE
-from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound, Template
 
-def render_template(source, content={}, path=None, searchpath=TEMPLATES_DIR, **envkwargs):
+def render_template(template_name_or_string, context={}, path=None, searchpath=TEMPLATES_DIR, **envkwargs):
+    """Render Template.
+
+    Args:
+        template_name_or_string (str) : The name of the template to be rendered or a string.
+        context (dict)                : The variables that should be available in the context of the template.
+        path (str)                    : If given, output to ``path`` file.
+        searchpath (str)              : (Default= ``TEMPLATES_DIR`` )
+
+    Examples:
+        >>> import matplotlib
+        >>> from pycharmers.utils import render_template
+        >>> render_template(
+        ...     template_name_or_string="fonts.html", 
+        ...     context={"fonts": sorted(set([f.name for f in matplotlib.font_manager.fontManager.ttflist]))}
+        >>> )
+            <h1>Available Fonts</h1>
+            <ul>
+            <li>.Aqua Kana: <span style='font-family:.Aqua Kana; font-size: 2em;'>.Aqua Kana</li>
+            <li>.Arabic UI Display: <span style='font-family:.Arabic UI Display; font-size: 2em;'>.Arabic UI Display</li>
+            : 
+    """
     env = Environment(loader=FileSystemLoader(searchpath=searchpath), **envkwargs)
     try:
-        template = env.get_template(name=source)
+        template = env.get_template(name=template_name_or_string)
     except TemplateNotFound:
-        template = env.from_string(source=source)
-    string = template.render(**content)
+        template = env.from_string(source=template_name_or_string)
+    string = template.render(**context)
     if path is None:
         print(string)
     else:
@@ -41,3 +62,51 @@ for fn in os.listdir(TEMPLATES_DIR):
     if fn[-5:] != ".html": continue
     name = fn.replace(".", "_")
     exec(f"{name} = _mk_func('{fn}')")
+
+class defFunction():
+    def __init__(self, func_name, short_description="", description="", is_method=False):
+        self.func_name = func_name
+        self.short_description = short_description
+        self.description = description
+        self.is_method = is_method
+        self.argwidth = 0
+        self.arguments = {}
+
+    def set_example(self, prefix="", suffix=""):
+        if prefix=="":
+            prefix = ">>> " + self.func_name + "("
+        self.example_prefix = prefix
+        self.example_suffix = suffix
+        self.examples=True
+
+    def set_returns(self, name, type, description=""):
+        self.returns = f"{name} ({type.__name__}): {description}"
+        
+    def sort_arguments(self):
+        self.arguments = dict(sorted(self.arguments.items(), key=lambda x: x[1]["is_required"], reverse=True))
+        
+    def set_argument(self, name, default, example=None, description="", type=str, is_required=True, **kwargs):
+        name_type = f"{name} ({type.__name__})"
+        argumeta = {
+            "type": type,
+            "is_required": is_required,
+            "default": default,
+            "example": example,
+            "description": description,
+            "name_type": name_type,
+        }
+        argumeta.update(kwargs)
+        self.arguments[name] = argumeta
+        self.argwidth = max(self.argwidth, len(name_type))
+        
+    def set_arguments(self, **arguments):
+        for argname, argmeta in arguments.items:
+            self.set_argument(name=argname, **argmeta)
+
+    def create(self):
+        self.sort_arguments()
+        render_template(
+            template_name_or_string="def.html",
+            context=self.__dict__,
+            searchpath=TEMPLATES_DIR,
+        )
