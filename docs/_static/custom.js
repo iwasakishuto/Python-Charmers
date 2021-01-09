175e2c772d14feeaac4a51a5efba31351ecb8d3e
iwasakishuto <cabernet.rock@gmail.com>
2021-01-10 01:35:55 +0900

    Added javascript to improve the appearance of ToC in sidebar.

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -4,18 +4,22 @@
     $("dl.py.method dt:has(em.property)").addClass("dt-property");
   });
 
-  var items = document.querySelectorAll('li[class*="toctree-"]');
-  items.forEach((item) => {
-    var text  = item.querySelector("a").innerHTML
-    var text_components = text.split(".");
-    var num_components = text_components.length;
-    if (num_components>0){
-      text = text_components[num_components-1];
-    }
-    text = text.replace(/\spackage/g,' <span style="color:greenyellow">package</span>')
-               .replace(/\smodule/g,'<span style="color:aqua">.py</span>')
-               .replace(/Subpackages/g,'<span style="color:white;text-decoration: underline;">Subpackages</span>')
-               .replace(/Submodules/g,'<span style="color:white:text-decoration: underline;">Submodules</span>');
-    item.querySelector("a").innerHTML = text
+  $(function(){
+
+    var items = document.querySelectorAll('div.sphinxsidebarwrapper li[class*="toctree-"]');
+    items.forEach((item) => {
+      var text  = item.querySelector("a").innerHTML
+      var text_components = text.split(".");
+      var num_components = text_components.length;
+      if (num_components>0){
+        text = text_components[num_components-1];
+      }
+      text = text.replace(/\spackage/g,' <span class="package-name">package</span>')
+                .replace(/(.*)\smodule/g, '<span class="program-name">$1.py</span>')
+                .replace(/(Subpackages|Submodules)/g,'<span class="package-subtitle">$1</span>');
+      item.querySelector("a").innerHTML = text
+    });
+
   });
 })(jQuery);
+
