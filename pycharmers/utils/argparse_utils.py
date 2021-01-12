c51c2cc07ddae77011334283e6d3930014a91ef6
iwasakishuto <cabernet.rock@gmail.com>
2021-01-12 17:44:06 +0900

    Supported List params of types other than str such as int and float

--- pycharmers/utils/argparse_utils.py
+++ pycharmers/utils/argparse_utils.py
@@ -1,33 +1,54 @@
 # coding: utf-8
 import re
 import argparse
+
+from ..__meta__ import __project_name__
 from .generic_utils import str_strip
 
-class ListParamProcessor(argparse.Action):
-    """Receive List arguments.
-    
+def ListParamProcessorCreate(type=str):
+    """Create a ListParamProcessor
+
+    Args:
+        type (type) : type of each element in list.
+
+    Returns:
+        ListParamProcessor (argparse.Action) : Processor which receives list arguments.
+
     Examples:
         >>> import argparse
-        >>> from pycharmers.utils import ListParamProcessor
+        >>> from pycharmers.utils import ListParamProcessorCreate
         >>> parser = argparse.ArgumentParser()
-        >>> parser.add_argument("--list_params", action=ListParamProcessor)
+        >>> parser.add_argument("--list_params", action=ListParamProcessorCreate())
         >>> args = parser.parse_args(args=["--list_params", "[あ, い, う]"])
         >>> args.list_params
         ['あ', 'い', 'う']
-
-    Note:
-        If you run from the command line, execute as follows::
-        
-        $ python app.py --list_params "[あ, い, う]"
-
     """
-    def __call__(self, parser, namespace, values, option_strings=None):
-        match = re.match(pattern=r"(?:\[|\()(.+)(?:\]|\))", string=values)
-        if match:
-            values = [str_strip(e) for e in match.group(1).split(",")]
-        else:
-            values = [values]
-        setattr(namespace, self.dest, values)
+    class ListParamProcessor(argparse.Action):
+        """Receive List arguments.
+        
+        Examples:
+            >>> import argparse
+            >>> from pycharmers.utils import ListParamProcessor
+            >>> parser = argparse.ArgumentParser()
+            >>> parser.add_argument("--list_params", action=ListParamProcessor)
+            >>> args = parser.parse_args(args=["--list_params", "[あ, い, う]"])
+            >>> args.list_params
+            ['あ', 'い', 'う']
+
+        Note:
+            If you run from the command line, execute as follows::
+            
+            $ python app.py --list_params "[あ, い, う]"
+
+        """
+        def __call__(self, parser, namespace, values, option_strings=None, **kwargs):
+            match = re.match(pattern=r"(?:\[|\()(.+)(?:\]|\))", string=values)
+            if match:
+                values = [type(str_strip(e)) for e in match.group(1).split(",")]
+            else:
+                values = [type(values)]
+            setattr(namespace, self.dest, values)
+    return ListParamProcessor
 
 class DictParamProcessor(argparse.Action):
     """Receive an argument as a dictionary.
@@ -84,3 +105,32 @@ class KwargsParamProcessor(argparse.Action):
     def __call__(self, parser, namespace, values, option_strings=None):
         k,v = values.split("=")
         setattr(namespace, k, v)
+
+def cv2ArgumentParser(parser=None, prog="", description=None, add_help=True, **kwargs):
+    """``ArgumentParser`` for OpenCV project.
+    
+    Args:
+        --winname (str)                     : Window name.
+        --path (str)                        : Path to video or image.
+        --cam (int)                         : The ID of the web camera.
+        --ext (str)                         : The extension for saved image.
+        --gui-width (int)                   : The width of the GUI tools.
+        --gui-margin (int)                  : The margin of GUI control tools.
+        --monitor-size (ListParamProcessor) : Monitor size. ( ``width`` , ``height`` )
+        --autofit (bool)                    : Whether to fit display size to window size.
+        --twitter (bool)                    : Whether you want to run for tweet. ( ``display_size`` will be () )
+        --capture (bool)                    : Whether you want to save as video.
+    """
+    if parser is None:
+        parser = argparse.ArgumentParser(prog=prog, description=description, add_help=add_help, **kwargs)
+    parser.add_argument("--winname",      type=str, default=prog+ f": {__project_name__}", help="Window name.")
+    parser.add_argument("--path",         type=str, help="Path to video or image.")
+    parser.add_argument("--cam",          type=int, default=0,   help="Define the id of the web camera. `cv2.VideoCapture( [ID] )`")
+    parser.add_argument("--gui-width",    type=int, default=200, help="The width of the GUI tools.")
+    parser.add_argument("--gui-margin",   type=int, default=10,  help="The margin of GUI control tools.")
+    parser.add_argument("--ext",          type=str, default=".jpg", help="The extension for saved image.")
+    parser.add_argument("--monitor-size", action=ListParamProcessorCreate(type=int), default=(1080, 720), help="Monitor size. (width, height)")
+    parser.add_argument("--autofit",      action="store_true", help="Whether to fit display size to window size.")
+    parser.add_argument("--twitter",      action="store_true", help="Whether you want to run for tweet. ( ``display_size`` will be ( ``1300`` , ``730`` ) ).")
+    parser.add_argument("--capture",      action="store_true", help="Whether you want to save as video.")
+    return parser
