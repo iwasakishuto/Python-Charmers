f910e32cd1b203f7ddf8c7c9d324c3d384ac711a
iwasakishuto <cabernet.rock@gmail.com>
2021-05-12 21:47:52 +0900

    🚶 Minor Updates

--- pycharmers/api/__init__.py
+++ pycharmers/api/__init__.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 from . import github
+from . import google_drive
 from . import slack
 
 
 from .github import url2raw
 from .github import wgit
+
+from .google_drive import PyCharmersGoogleDrive
 
 from .slack import SlackClient
